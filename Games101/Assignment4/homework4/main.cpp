5366a3233af32e3a9089170325e23017eb41f830
Juanjuan <liujuanjuan134@foxmail.com>
2021-07-13 22:58:16 +0800

    Games101-作业4 De Casteljau算法（递归计算、代数计算）

--- Games101/Assignment4/homework4/main.cpp
+++ Games101/Assignment4/homework4/main.cpp
@@ -30,10 +30,37 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
     }
 }
 
+// Bezier Curve - Algebraic Formula
+cv::Point2f algebraic_bezier(const std::vector<cv::Point2f> &control_points, float t) 
+{
+    cv::Point2f bezierpoint(0.f, 0.f);
+    auto n = control_points.size() - 1;
+    for (size_t i = 0; i <= n; ++i)
+    {
+        auto Cni = std::tgamma(n + 1) / (std::tgamma(i + 1) * std::tgamma(n - i + 1));
+        auto Bni = Cni * std::pow((1 - t), i) * std::pow(t, n - i);
+        bezierpoint += control_points[i] * Bni;
+    }
+    return bezierpoint;
+}
+
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+
+    std::vector<cv::Point2f> de_points = control_points;
+    std::function<void(size_t)> recursiveFunc = [&](size_t control_count)
+    {
+        if (control_count == 1) return;
+
+        // de_points[0..control_count] => de_points[0..control_count-1]
+        for (size_t i = 0; i < control_count - 1; ++i)
+            de_points[i] = de_points[i] + (de_points[i + 1] - de_points[i]) * t;
+
+        recursiveFunc(control_count - 1);
+    };
+    recursiveFunc(de_points.size());
+    return de_points[0];
 
 }
 
@@ -42,6 +69,12 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
 
+    for (double t = 0.0; t <= 1.0; t += 0.001)
+    {
+        auto point = recursive_bezier(control_points, t);
+        //auto point = algebraic_bezier(control_points, t);
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+    }
 }
 
 int main() 
@@ -62,8 +95,8 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            //naive_bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);

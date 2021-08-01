a2d96639070b841ec939a4d9a1532c73746081a2
Juanjuan <liujuanjuan134@foxmail.com>
2021-08-01 23:43:51 +0800

    Games101-作业5 生成光线

--- Games101/Assignment5/Assignment5/Renderer.cpp
+++ Games101/Assignment5/Assignment5/Renderer.cpp
@@ -223,14 +223,15 @@ void Renderer::Render(const Scene& scene)
         for (int i = 0; i < scene.width; ++i)
         {
             // generate primary ray direction
-            float x;
-            float y;
+            float x = ((i + 0.5f) / scene.width - 0.5f) * 2 * imageAspectRatio;
+            float y = 1.0f - 2.0f * (j + 0.5f) / scene.height;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            dir = normalize(dir);
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
---
+++ Games101/Assignment5/NOTES.md
@@ -0,0 +1,40 @@
+> Games101-5
+
+# 环境-VS2019作业5
+
+```
+D8021	无效的数值参数“/Wextra”
+-Wshadow
+-Wreturn-type
+```
+
+GCC编译选项-Wextra
+
+vs2019 cl编译器尝试使用
+
+直接F5，RTCu运行时检查的原因
+
+```
+Run-Time Check Failure #3 - The variable 'y' is being used without being initialized.
+```
+
+支持浏览ppm的软件
+
+http://openseeit.sourceforge.net/
+
+参考：
+
+https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-generating-camera-rays/generating-camera-rays
+
+# 作业
+
+## 构造光线
+
+假设：屏幕为xoy平面上，z=-1的长方形，高度为2，即y方向上[-1, 1]
+
+求wh位置的点坐标
+
+现象：
+
+- 光线方向没有归一化的情况下，前面的球有黑边
+- 

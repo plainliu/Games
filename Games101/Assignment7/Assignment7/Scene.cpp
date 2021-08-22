8cef19c268e2109fd84dde0c1610d9275d758a3a
Juanjuan <liujuanjuan134@foxmail.com>
2021-08-22 23:07:15 +0800

    Games101-作业7 PathTracing 直接光照部分-采样光源

--- Games101/Assignment7/Assignment7/Scene.cpp
+++ Games101/Assignment7/Assignment7/Scene.cpp
@@ -60,5 +60,52 @@ bool Scene::trace(
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
-    // TO DO Implement Path Tracing Algorithm here
+    // TO DO Implement Path Tracing Algorithm hereshade(p, wo)
+    if ( depth > this->maxDepth )
+        return Vector3f( 0.0, 0.0, 0.0 );
+
+    Intersection p = Scene::intersect( ray );
+    if ( p.happened == false )
+        return Vector3f(0.f);
+
+    Material* m = p.m;
+    Vector3f P = p.coords;
+    Vector3f N = p.normal;
+
+    // Test Normal
+    //hitColor = (N + 1.0f) * 0.5f;
+
+    Vector3f lightdir(0.f);
+
+    Intersection interLight;
+    float pdf_light = 0.0f;
+    //sampleLight( inter, pdf_light )
+    //Get x, ws, NN, emit from inter
+    sampleLight( interLight, pdf_light );
+    //Shoot a ray from p to x
+    Vector3f ws = ( interLight.coords - P ).normalized( );
+    Ray r( p.coords, ws );
+    //If the ray is not blocked in the middle
+    Intersection hitLight = intersect( r );
+    if ( hitLight.obj && hitLight.obj->hasEmit( ) == true )
+    {
+        //L_dir = emit * eval( wo, ws, N ) * dot( ws, N ) * dot( ws,
+        //    NN ) / |x - p | ^ 2 / pdf_light
+        lightdir = interLight.emit * m->eval( -ray.direction, ws, N )
+            * dotProduct( ws, N) * dotProduct( -ws, interLight.normal)
+            / pow( ( P - interLight.coords ).norm(), 2 ) / pdf_light;
+    }
+
+    //L_indir = 0.0
+    Vector3f L_indir( 0.f );
+
+    //Test Russian Roulette with probability RussianRoulette
+    //wi = sample( wo, N )
+    //Trace a ray r( p, wi )
+    //If ray r hit a non - emitting object at q
+    //L_indir = shade( q, wi ) * eval( wo, wi, N ) * dot( wi, N )
+    /// pdf( wo, wi, N ) / RussianRoulette
+
+    //Return L_dir + L_indir
+    return lightdir + L_indir;
 }
